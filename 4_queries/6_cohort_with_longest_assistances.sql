71839fd28a3a30b2d6a9f7c273a65e380c19fb6e
Heather <hmcleod29@gmail.com>
2022-05-31 20:01:59 -0400

    Last round of queries

---
+++ 4_queries/6_cohort_with_longest_assistances.sql
@@ -0,0 +1,7 @@
+SELECT cohorts.name, AVG(assistance_requests.completed_at - assistance_requests.started_at) as average_assistance_request_time
+FROM cohorts 
+JOIN students ON cohorts.id = students.cohort_id
+JOIN assistance_requests ON students.id = assistance_requests.student_id
+GROUP BY cohorts.name
+ORDER BY average_assistance_request_time DESC
+LIMIT 1;
